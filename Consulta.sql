08fb01f6c22fe63154d7eba2bc4e0b9ff3c9c35c
MariahBitencourt <mariahhelena.bitencourt@gmail.com>
2024-06-10 17:20:39 -0300

    Heidi,MySQL e Banco de Dados

---
+++ Consulta.sql
@@ -0,0 +1,13 @@
+/*Mostrar banco de dados*/
+SHOW DATABASES
+/*Selecionar bancos de dados*/
+USE mysql
+/*Mostrar tabelas*/
+SHOW TABLES
+/*Criar banco de dados*/
+CREATE DATABASE senabook
+/*Apagar banco de dados*/
+DROP DATABASE test
+
+
+
